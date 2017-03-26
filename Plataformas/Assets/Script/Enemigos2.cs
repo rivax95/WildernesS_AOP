f90f9d52ab0cac3ced84272831dbd14708ddf55c
rivax95 <alejandrotejemundos@hotmail.es>
2017-03-26 11:50:28 +0200

    Script enemigo
    Script enemigo v0.0.1 (No terminado)

--- Plataformas/Assets/Script/Enemigos2.cs
+++ Plataformas/Assets/Script/Enemigos2.cs
@@ -7,10 +7,12 @@ public class Enemigos2 : MonoBehaviour {
     GameObject player;
     bool derecha;
     public float rango = 4f;
-    LayerMask raycastdetected;
+    LayerMask raycastdetected; // NOTE CONFIGURAR EN UNITY
     RaycastHit2D detectde;
     RaycastHit2D detectiz;
     Vector2 objetibo;
+    public float distancia_ataque=0.5f;
+    public int dano = 1;
     public float VelMov = 0.5f;
     // Use this for initialization
     void Start () {
@@ -22,16 +24,24 @@ public class Enemigos2 : MonoBehaviour {
          detectiz = Physics2D.Raycast(transform.position, transform.right * (-1),rango,raycastdetected);
          detectde = Physics2D.Raycast(transform.position, transform.right * (1), rango, raycastdetected);
         objetibo = player.gameObject.transform.position;
+        perseguir();
     }
     public void perseguir()
-    {
+    { // NOTE mirar bien el script de movimiento que no interrumpa a este ni viceversa.
         if (detectde.collider.tag == "Player")
         {
+            Debug.Log("Enemigo detecta a Player");
             this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
             transform.position += new Vector3(+1f, 0f, 0f) * VelMov * Time.deltaTime;
-            if (detectde.distance == 1)
+            if (detectde.distance == distancia_ataque)
             {
-
+                //NOTE Cuando se hacerque se tiene que quedar parado
+                transform.position = transform.position;
+                //NOTE Primero un culdown y al termianr el ataque otro
+                Debug.Log("Enemigo se dispone a atacar");
+                //TODO ejecuta animacion
+                //TODO CULDOWN ATAQUE
+                player.GetComponent<PlayerData>().vidas -= dano;
             }
         }
     }
